7bc4695d30b1397976af7f3c7dcea71360c8d716
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-05-18 00:04:29 +0900

    create operation log

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,7 +1,13 @@
 import React, { useState, useContext } from "react";
 
-import { CREATE_EVENT, DELETE_ALL_EVENT } from "../actions";
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENT,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -9,11 +15,15 @@ const EventForm = () => {
   const [body, setBody] = useState("");
   const addEvent = (e) => {
     e.preventDefault();
-    console.log({ title, body });
     dispatch({
       type: CREATE_EVENT,
       title,
       body
+    });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました。",
+      operatedAt: timeCurrentIso8601()
     });
     setTitle("");
     setBody("");
@@ -23,7 +33,15 @@ const EventForm = () => {
     const result = window.confirm(
       "全てのイベントを本当に削除してもいいですか？"
     );
-    if (result) dispatch({ type: DELETE_ALL_EVENT });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENT });
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "全てのイベントを削除しました。",
+        operatedAt: timeCurrentIso8601()
+      });
+    }
   };
   const unCreatable = title === "" || body === "";
   return (
